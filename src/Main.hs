288b78a9eba75480c2c7a0d388dc139d0229578e
Ilham <jangantanyaaku14@gmail.com>
2024-12-09 00:14:50 +0800

    feat: json hello world and echo server

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# language DeriveAnyClass #-}
+{-# language ScopedTypeVariables #-}
 
 module Main (main) where
 
 import Web.Scotty
+import Data.Aeson (decode, object, Value, (.=))
 
 main :: IO ()
 main = scotty 7001 $ do
-  get "/" $ text "Hello, World!"
+  get "/" $ json $ object ["hello" .= ("world" :: String)]
+  post "/echo" $ do
+    reqBody <- body
+    case (decode reqBody :: Maybe Value) of
+      Just item -> json item
+      Nothing -> json $ object ["error" .= ("Something went wrong" :: String)]
