989dd0d2560e23802563223fc471f58964677693
Ilham <jangantanyaaku14@gmail.com>
2024-12-10 18:42:31 +0800

    feat: db connect

--- src/Main.hs
+++ src/Main.hs
@@ -5,12 +5,30 @@ module Main (main) where
 
 import Web.Scotty
 import Data.Aeson (decode, object, Value, (.=))
+import Configuration.Dotenv (loadFile, defaultConfig)
+import System.Environment (getEnv)
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Database.MySQL.Simple
 
 main :: IO ()
-main = scotty 7001 $ do
-  get "/" $ json $ object ["hello" .= ("world" :: String)]
-  post "/echo" $ do
-    reqBody <- body
-    case (decode reqBody :: Maybe Value) of
-      Just item -> json item
-      Nothing -> json $ object ["error" .= ("Something went wrong" :: String)]
+main = do
+  loadFile defaultConfig
+
+  mysqlUser <- getEnv "MYSQL_USER"
+  mysqlPassword <- getEnv "MYSQL_PASSWORD"
+  mysqlDatabase <- getEnv "MYSQL_DATABASE"
+  conn <- connect
+        defaultConnectInfo {connectUser = mysqlUser, connectPassword = mysqlPassword, connectDatabase = mysqlDatabase}
+
+  scotty 7001 $ do
+
+    get "/" $ do 
+      json $ object ["hello" .= ("world" :: String)]
+    post "/echo" $ do
+      reqBody <- body
+      case (decode reqBody :: Maybe Value) of
+        Just item -> json item
+        Nothing -> json $ object ["error" .= ("Something went wrong" :: String)]
+    get "/rand" $ do
+      [test] <- liftIO $ query_ conn "SELECT RAND()" :: ActionM [Only Float]
+      json $ object ["rand" .= fromOnly test]
