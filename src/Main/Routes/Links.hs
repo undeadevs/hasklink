3bd35d2152db0d50664607c384675716a3712960
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-12-11 04:54:13 +0800

    feat: links endpoint

--- src/Main/Routes/Links.hs
+++ src/Main/Routes/Links.hs
@@ -1,42 +1,168 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# language ScopedTypeVariables #-}
 
-module Main.Routes.Links (getLinks, createLink, getLinkDetail, deleteLinks, updateLinks, hitLink) where
+module Main.Routes.Links (getLinks, createLink, getLinkDetail, deleteLinks, updateLinks, hitLink, extendExpire) where
 
-import Data.Aeson (decode, object, Value, (.=))
+import Data.Aeson (decode, object, (.=), (.:), (.:?),Object, Value(..), withObject)
 import Web.Scotty
+import Data.Aeson.Types (parseEither, Parser, Object)
+import Main.Connect
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Database.MySQL.Simple
+import Data.Text.Lazy (Text,fromStrict)
+import Data.Maybe (fromMaybe)
+import Web.Scotty.Cookie
+import Data.Time.Clock (UTCTime, addUTCTime,getCurrentTime )
+
+
 
 createLink :: ScottyM ()
-createLink =
-    post "/links" $ do
-      json $ object ["hello" .= ("world" :: String)]    
-        
+createLink = post "/links" $ do
+    reqBody <- body
+    conn <- liftIO getConnection
+    user_id <- getCookie "user_id"
+    case decode reqBody of
+        Just (Object obj) -> do
+            let parseFields :: Object -> Parser (Text, Text)
+                parseFields obj = do
+                    alias <- obj .: "alias"
+                    originalUrl <- obj .: "original_url"
+                    return (alias, originalUrl)
+            case parseEither parseFields obj of
+                Right (alias, originalUrl) -> do
+                    liftIO $ execute conn
+                        "INSERT INTO links (owner_id, alias, original_url) VALUES (?, ?, ?)"
+                        (user_id, alias, originalUrl)
+                    json $ object ["status" .= ("success" :: Text), "alias" .= alias, "original_url" .= originalUrl]
+                Left err -> do
+                    json $ object ["error" .= ("Invalid request body" :: Text), "details" .= err]
+        _ -> do
+            json $ object ["error" .= ("internal server error" :: Text)]
 
 getLinks :: ScottyM ()
-getLinks =
-    get "/links" $ do
-      json $ object ["hello" .= ("world" :: String)]    
+getLinks = get "/links" $ do
+    conn <- liftIO getConnection
+    maybeUserId <- getCookie "user_id" 
+    case maybeUserId of
+        Just userId -> do
+            result <- liftIO $ query conn
+                "SELECT alias, original_url, click_count FROM links WHERE owner_id = ?"
+                (Only userId) :: ActionM [(Text, Text, Int)]
+            json $ map (\(alias, originalUrl, clickCount) -> 
+                          object ["alias" .= alias, 
+                                  "original_url" .= originalUrl, 
+                                  "click_count" .= clickCount]) result
+        Nothing -> do
+            json $ object ["error" .= ("log in first" :: Text)]
 
 getLinkDetail :: ScottyM ()
 getLinkDetail =
-    get "/links" $ do
-      json $ object ["hello" .= ("world" :: String)]    
+    get "/links/:slug" $ do
+        slug <- captureParam "slug" :: ActionM Text
+        conn <- liftIO getConnection
+        result <- liftIO $ query conn
+            "SELECT alias, original_url, click_count, expires_at FROM links WHERE alias = ?"
+            (Only slug) :: ActionM [(Text, Text, Int,  Maybe UTCTime)]
+        case result of
+            [(alias, originalUrl, clickCount, expiresAt)] -> do
+                json $ object
+                    [ "alias" .= alias
+                    , "original_url" .= originalUrl
+                    , "click_count" .= clickCount
+                    , "expires_at" .= expiresAt
+                    ]
+            _ -> do
+                json $ object ["error" .= ("Link not found" :: Text)]
 
 
 deleteLinks :: ScottyM ()
 deleteLinks =
     delete "/links/:slug" $ do
-      json $ object ["hello" .= ("world" :: String)]    
-
-
+        slug <- captureParam "slug" :: ActionM String 
+        user_id <- getCookie "user_id" 
+        conn <- liftIO getConnection         
+        rows <- liftIO $ execute conn 
+            "DELETE FROM links WHERE alias = ?" 
+            (Only slug)  
+        if rows > 0
+            then do
+                json $ object ["message" .= ("Link deleted successfully" :: String)]
+            else do
+                json $ object ["error" .= ("Link not found" :: String)]
+   
 
 updateLinks :: ScottyM ()
-updateLinks =
+updateLinks = 
     put "/links/:slug" $ do
-      json $ object ["hello" .= ("world" :: String)]    
+        slug <- captureParam "slug" :: ActionM Text
+        reqBody <- body 
+        conn <- liftIO getConnection 
+        case decode reqBody of
+            Just (Object obj) -> do
+                let parseFields :: Object -> Parser (Text, Text)
+                    parseFields obj = do
+                        alias <- obj .: "alias"
+                        originalUrl <- obj .: "original_url"
+                        return (alias, originalUrl)
+                case parseEither parseFields obj of
+                    Right (alias, originalUrl) -> do
+                        liftIO $ execute conn
+                            "UPDATE links SET alias = ?, original_url = ? WHERE alias = ?"
+                            (alias, originalUrl, slug)
+                        json $ object ["status" .= ("success" :: Text), "alias" .= alias, "original_url" .= originalUrl]
+                    Left err -> 
+                        json $ object ["error" .= ("Invalid request body" :: Text), "details" .= err]
+            _ -> 
+                json $ object ["error" .= ("Internal server error" :: Text)]
 
 
 hitLink :: ScottyM ()
 hitLink = 
     get "/:slug" $ do
-      json $ object ["hello" .= ("world" :: String)]    
+        slug <- captureParam "slug" :: ActionM String  
+        conn <- liftIO getConnection
+        _ <- liftIO $ execute conn 
+            "UPDATE links SET click_count = click_count + 1 WHERE alias = ?" 
+            (Only slug)
+        result <- liftIO $ query conn 
+            "SELECT original_url, expires_at FROM links WHERE alias = ?" 
+            (Only slug) :: ActionM [(Text, Maybe UTCTime)]
+
+        case result of
+            [(originalUrl, Just expiresAt)] -> do
+                currentTime <- liftIO getCurrentTime
+                if currentTime > expiresAt
+                    then do
+                        html $ mconcat ["<h1>URL HAS BEEN EXPIRED!</h1>"]
+                    else redirect originalUrl  
+            [(originalUrl, Nothing)] -> redirect originalUrl
+            [] -> do
+                json $ object ["error" .= ("Slug not found" :: Text)]
+            _ -> do
+                json $ object ["error" .= ("Unexpected error" :: Text)]
+
+
+extendExpire :: ScottyM ()
+extendExpire =
+    put "/links/:slug/extend" $ do
+        slug <- captureParam "slug"  :: ActionM Text
+        reqBody <- body 
+        conn <- liftIO getConnection 
+        case decode reqBody of
+            Just (Object obj) -> do
+                case parseEither (.:? "extend_time") obj of
+                    Right (Just extendTime) -> case extendTime of
+                        Number hours -> do
+                            let extendHours = truncate hours :: Int -- Convert to Int safely
+                            liftIO $ execute conn
+                                "UPDATE links SET expires_at = DATE_ADD(expires_at, INTERVAL ? HOUR) WHERE alias = ?"
+                                (extendHours, slug)
+                            json $ object ["status" .= ("success" :: Text), "extended_by_hours" .= extendHours]
+                        _ -> json $ object ["error" .= ("'extend_time' must be a number" :: Text)]
+                    Right Nothing ->
+                        json $ object ["error" .= ("Invalid request body" :: Text)]
+                    Left err ->
+                        json $ object ["error" .= ("Internal server error'" :: Text)]
+            _ -> 
+                json $ object ["error" .= ("Internal server error" :: Text)]
+
